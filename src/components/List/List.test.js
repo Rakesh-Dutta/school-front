3aacd97de48615902c4d5429f26064699497089b
RakeshArya07 <rakesharya.nitdgp@gmail.com>
2017-02-06 15:42:44 +0530

    List of boxes and tests

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -4,8 +4,20 @@ import {expect} from 'chai';
 import sinon from 'sinon';
 
 import List from './List'; //importing js file from name List
+import Box from '../Box/Box'
+
+let Students;
 
 describe('List component ', () => {
+
+    beforeEach(()=>{
+    Students = [
+            {id:3, text:"bob", css:"selected"},
+            {id:4, text:"sam", css:"empty"},
+            {id:5, text:"frank", css:"selected"}
+        ]
+    });
+
     it('Should render without error', () => {
         const wrapper = shallow(<List />);
         expect(wrapper).to.be.ok;
@@ -22,22 +34,20 @@ describe('List component ', () => {
     });
 
     it('should render out 3 boxes', () => {
-        const Students = [
-            {id:3, text:"bob", css:"selected"},
-            {id:4, text:"test", css:"empty"},
-            {id:5, text:"frank", css:"selected"}
-        ]
         const wrapper = mount(<List header="Students" items={Students}/>);
         expect(wrapper.find('.box').length).to.equal(3); 
     });
 
-    it('should render out 2 boxes', () => {
-        const Students = [
-            {id:3, text:"bob", css:"selected"},
-            {id:4, text:"test", css:"empty"}
-        ]
+    it('should display sam in the 2 boxes', () => {
         const wrapper = mount(<List header="Students" items={Students}/>);
-        expect(wrapper.find('.box').length).to.equal(2); 
-        console.log(wrapper.find('.box').at(0).html());  
+        expect(wrapper.find(Box).length).to.equal(3); 
+        expect(wrapper.find(Box).at(1).find('div > div').html()).to.equal('<div data-id="4" class="empty">sam</div>');  
+    });
+
+    it('should call function when 2nd box is clicked', () => {
+        const stub = sinon.stub();
+        const wrapper = mount(<List click={stub} header="Students" items={Students}/>);
+        wrapper.find(Box).at(1).find('div > div').simulate('click');
+        expect(stub.callCount).to.equal(1);
     });
 });
